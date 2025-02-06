e0d5d6835268e18b7a8040607eed4a1fdbd0fa98
Soura Sankar Mondal <soura.sankar@ibm.com>
2025-02-06 15:29:40 +0530

    click event added

--- blocks/carousal/carousal.js
+++ blocks/carousal/carousal.js
@@ -1,23 +1,30 @@
 export default function decorate(block) {
     console.log(block);
     const rows = [...block.children];
+
     [...block.children].forEach((row, r) => {
         if (r == 0) {
             const nextBtn = document.createElement('button');
-            nextBtn.classList.add('btn');
-            nextBtn.classList.add('btn-next');
-            const node = document.createTextNode(row.textContent)
-            console.log(node);
+            nextBtn.classList.add('btn', 'btn-next');
+            const node = document.createTextNode(row.textContent);
             nextBtn.append(node);
             row.replaceWith(nextBtn);
+
+            // Add click event listener
+            nextBtn.addEventListener('click', () => {
+                console.log('Next button clicked');
+            });
         }
+        
         if (r == rows.length - 1) {
             const prevBtn = document.createElement('button');
-            prevBtn.classList.add('btn');
-            prevBtn.classList.add('btn-prev');
-            const node = document.createTextNode(row.textContent)
+            prevBtn.classList.add('btn', 'btn-prev');
+            const node = document.createTextNode(row.textContent);
             prevBtn.append(node);
             row.replaceWith(prevBtn);
+            prevBtn.addEventListener('click', () => {
+                console.log('Prev button clicked');
+            });
         }
     });
 }

f68129196362e4531316da08e41a22d330292da7
Soura Sankar Mondal <soura.sankar@ibm.com>
2025-02-06 15:03:46 +0530

    test

--- blocks/carousal/carousal.js
+++ blocks/carousal/carousal.js
@@ -1,1 +1,7 @@
-alert("testJs");
+export default function decorate(block) {    
+    [...block.children].forEach((row,i) => {
+        [...row.children].forEach((col,j) => {
+            alert(i+","+j);
+        });
+    });
+  }

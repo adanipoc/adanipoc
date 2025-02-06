3ea4fed81cf425e1bab8e401ea894c606098c05d
Soura Sankar Mondal <soura.sankar@ibm.com>
2025-02-06 15:20:00 +0530

    jhgg

--- blocks/carousal/carousal.js
+++ blocks/carousal/carousal.js
@@ -1,7 +1,19 @@
-export default function decorate(block) {    
-    [...block.children].forEach((row,i) => {
-        [...row.children].forEach((col,j) => {
-            alert(i+","+j);
-        });
+export default function decorate(block) {
+    console.log(block);
+    const rows = [...block.children];
+    [...block.children].forEach((row, r) => {
+        if (r == 0) {
+            const nextBtn = document.createElement('button');
+            nextBtn.classList.add('btn');
+            nextBtn.classList.add('btn-next');
+            const node = document.createTextNode(row.textContent)
+            nextBtn.append(node)
+        }
+        if (r == rows.length - 1) {
+            const prevBtn = document.createElement('button');
+            nextBtn.classList.add('btn');
+            nextBtn.classList.add('btn-prev');
+            const node = document.createTextNode(row.textContent)
+        }
     });
-  }
+}

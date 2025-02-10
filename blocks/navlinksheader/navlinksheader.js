86a8df7118f38649a008e40a00a86755e5426277
Amit Deshpande <amit.deshpande1@ibm.com>
2025-02-10 15:03:24 +0530

    dfdf

--- blocks/navlinksheader/navlinksheader.js
+++ blocks/navlinksheader/navlinksheader.js
@@ -7,12 +7,12 @@ export default function decorate(block) {
 		if (r == 0) {
 			const sliderDiv = document.createElement('div');
 			sliderDiv.classList.add('logo');
+
 			const newImage = document.createElement('img');
 			newImage.src = row.src; // Replace with your image URL
 			newImage.alt = 'Description of the image'; // Add alt text for accessibility
-
 			// Append the image to sliderDiv
-			sliderDiv.appendChild(row);
+			sliderDiv.appendChild(newImage);
 
 			// Replace the original row with the new sliderDiv
 			row.replaceWith(sliderDiv);

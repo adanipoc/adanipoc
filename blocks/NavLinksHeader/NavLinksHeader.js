bdc87b04842917151a62a18074a26e731558c7f4
Amit Deshpande <amit.deshpande1@ibm.com>
2025-02-10 14:41:33 +0530

    add

--- blocks/NavLinksHeader/NavLinksHeader.js
+++ blocks/NavLinksHeader/NavLinksHeader.js
@@ -1,10 +1,9 @@
-import { loadHeader } from "../../scripts/aem";
-
 export default function decorate(block) {
 	console.log(block);
 	const rows = [...block.children];
 
 	rows.forEach((row, r) => {
+		console.log(row);
 		if (r == 0) {
 			const sliderDiv = document.createElement('div');
 			sliderDiv.classList.add('NavLinksHeader');

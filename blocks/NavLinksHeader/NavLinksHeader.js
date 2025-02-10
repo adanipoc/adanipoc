62a9bdfca3ede5585f7bdb19171dc66007dfbcdf
Amit Deshpande <amit.deshpande1@ibm.com>
2025-02-10 14:36:28 +0530

    JS Changes

---
+++ blocks/NavLinksHeader/NavLinksHeader.js
@@ -0,0 +1,17 @@
+export default function decorate(block) {
+	console.log(block);
+	const rows = [...block.children];
+
+	rows.forEach((row, r) => {
+		if (r == 0) {
+			const sliderDiv = document.createElement('div');
+			sliderDiv.classList.add('NavLinksHeader');
+			row.replaceWith(sliderDiv);
+
+			// Add click event listener
+			sliderDiv.addEventListener('click', () => {
+				console.log('Next button clicked');
+			});
+		}
+	})
+}

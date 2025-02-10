b30949503115f19317b33124e680c80e1d1579ba
Amit Deshpande <amit.deshpande1@ibm.com>
2025-02-10 14:54:33 +0530

    iamge change

--- blocks/navlinksheader/navlinksheader.js
+++ blocks/navlinksheader/navlinksheader.js
@@ -3,10 +3,18 @@ export default function decorate(block) {
 	const rows = [...block.children];
 
 	rows.forEach((row, r) => {
+		console.log(row, r);
 		if (r == 0) {
 			const sliderDiv = document.createElement('div');
-			sliderDiv.classList.add('navlinksheader', 'testClass');
+			sliderDiv.classList.add('logo');
+			const newImage = document.createElement('img');
+			newImage.src = row.src; // Replace with your image URL
+			newImage.alt = 'Description of the image'; // Add alt text for accessibility
 
+			// Append the image to sliderDiv
+			sliderDiv.appendChild(newImage);
+
+			// Replace the original row with the new sliderDiv
 			row.replaceWith(sliderDiv);
 		}
 	});

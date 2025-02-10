3abacd41bd21d637447c85b5818c9889eca402dd
Amit Deshpande <amit.deshpande1@ibm.com>
2025-02-10 17:28:53 +0530

    asdf

--- blocks/bannercarousal/bannercarousal.js
+++ blocks/bannercarousal/bannercarousal.js
@@ -2,9 +2,9 @@ export default function decorate(block) {
 	const rows = [...block.children];
 	rows.forEach((item, index) => {
 		console.log(item);
-		const elment = document.createElement('div');
+		const slide = document.createElement('div');
+		slide.classList.add('slide');
 		const textWrapper = document.createElement('div');
-		elment.classList.add('slide');
 		textWrapper.classList.add('textWrapper');
 		const cols = [...item.children];
 		const heading = document.createElement('p');
@@ -17,14 +17,16 @@ export default function decorate(block) {
 			console.log(innerItem);
 			if(i == 0){
 				heading.textContent = innerItem.querySelector('p').textContent;
+				textWrapper.appendChild(heading);
 			}
 			if(i == 1){
 				subHeading.textContent = innerItem.querySelector('p').textContent;
+				textWrapper.appendChild(subHeading);
 			}
 			// innerItem.replaceWith()
-			// textWrapper.appendChild(innerItem);
 		});
-		item.replaceWith(elment);
+		slide.appendChild(textWrapper);
+		item.replaceWith(slide);
 
 	});
 }

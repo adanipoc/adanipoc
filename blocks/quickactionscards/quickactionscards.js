d844ec8680b37c24cb8d9cc3ff5d02925d4e954b
Amit Deshpande <amit.deshpande1@ibm.com>
2025-02-11 12:34:18 +0530

    js Changes

--- blocks/quickactionscards/quickactionscards.js
+++ blocks/quickactionscards/quickactionscards.js
@@ -1,7 +1,6 @@
 export default function decorate(block) {
 	const rows = [...block.children];
 	rows.forEach((item, index) => {
-		console.log(item);
 		const quickLinkItem = document.createElement('div');
 		quickLinkItem.classList.add('itemWrapper');
 		const children = [...item.children];

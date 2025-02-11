0b22466548dc7f41cf40d4267a854e5565905826
Amit Deshpande <amit.deshpande1@ibm.com>
2025-02-11 17:03:23 +0530

    File Name Changes

--- blocks/carousalcards/carousalcards.js
+++ blocks/carousalcards/carousalcards.js
@@ -2,6 +2,10 @@ export default function decorate(block) {
 	const rows = [...block.children];
 	rows.forEach((item, index) => {
 		if (index == 0) {
+			const cardImage = document.createElement('img');
+			
+		}
+		if (index == 1) {
 			const headingElement = document.createElement('p');
 			headingElement.classList.add('heading');
 			headingElement.textContent = item.textContent;

5c9436d4d2978f5ce3f10162d39fdd261598160b
Soura Sankar Mondal <soura.sankar@ibm.com>
2025-02-14 17:07:10 +0530

    test

--- blocks/tabcontent/tabcontent.js
+++ blocks/tabcontent/tabcontent.js
@@ -4,8 +4,48 @@ export default function decorate(block) {
     tabContent.classList.add('tabContent');
     const tabList = document.createElement('ul');
     tabList.classList.add('tabList');
+    var dataIndex = -1;
+    const content = document.createElement('div');
+    content.classList.add('content');
 	rows.forEach((row, r) => {
-        console.log(row);
-        
+        const cols = [...row.children];
+        const tabItem = document.createElement('li');
+        tabItem.classList.add('tabItem');
+        const contentItem = document.createElement('div');
+        contentItem.classList.add('contentItem');
+        const itemCard = document.createElement('div');
+        itemCard.classList.add('itemCard');
+        const img = document.createElement('img');
+        const title = document.createElement('p');
+        const subHeading = document.createElement('p');
+        img.setAttribute('width','46px');
+        img.setAttribute('height','46px');
+        title.className.add('title');
+        subHeading.classList.add('subHeading');
+        cols.forEach((col, c) => {
+            if(cols.length == 1){
+                dataIndex++;
+                tabItem.textContent = col.querySelector('p').textContent;
+                tabItem.setAttribute('data-index',dataIndex);
+                if(dataIndex == 0){
+                    tabItem.classList.add('active');
+                }
+                tabList.appendChild(tabItem);
+            }else{
+                if(c==0){
+                    img.src = col.querySelector('img').src;
+                    itemCard.appendChild(img);
+                }
+                if(c==1){
+                    title.textContent = col.querySelector('p').textContent;
+                    itemCard.appendChild(title);
+                }
+                if(c==2){
+                    subHeading.textContent = col.querySelector('p').textContent;
+                    itemCard.appendChild(subHeading);
+                }
+            }
+        });
+        console.log(itemCard);
 	});
 }

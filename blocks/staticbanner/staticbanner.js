784e3b798ef0a85069b1ac74de9401cd4ec54aa8
Soura Sankar Mondal <soura.sankar@ibm.com>
2025-02-17 16:06:55 +0530

    test

---
+++ blocks/staticbanner/staticbanner.js
@@ -0,0 +1,12 @@
+export default function decorate(block) {
+
+    const banner = document.createElement('div');
+    banner.classList.add('banner');
+    const image = document.createElement('img');
+    image.classList.add('bannerImage');
+    image.setAttribute('width','100%');
+    image.src = block.querySelector('img').src;
+    banner.appendChild(image);
+    block.innerHTML = '';
+    block.appendChild(banner);
+}

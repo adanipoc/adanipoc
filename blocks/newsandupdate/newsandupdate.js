f79ad51bcc183fb1f84b5ca84a4bd79fbc62d6e4
Soura Sankar Mondal <soura.sankar@ibm.com>
2025-02-17 14:22:55 +0530

    test

--- blocks/newsandupdate/newsandupdate.js
+++ blocks/newsandupdate/newsandupdate.js
@@ -24,10 +24,51 @@ export default function decorate(block) {
                 viewAll.textContent = col.querySelector('a').textContent;
                 viewAll.href = col.querySelector('a').href;
                 leftLayout.appendChild(viewAll);
-                console.log(leftLayout);
+            }
+            if(c==1){
+                const image = document.createElement('img');
+                image.setAttribute('width','186px');
+                image.setAttribute('alt','adani');
+                image.src = col.querySelector(img).src;
+                rightLayout.appendChild(image);
+                const updates = document.createElement('div');
+                updates.classList.add('updates');
+                const quote = document.createElement('img');
+                quote.classList.add('quote');
+                quote.src = '../../quote.png';
+                updates.appendChild('quote');
+                const ul = col.querySelector('ul');
+                ul.querySelectorAll('li').forEach((li, i) => {
+                    if(i==0){
+                        const heading = document.createElement('p');
+                        heading.classList.add('heading');
+                        heading.textContent = li.textContent;
+                        updates.appendChild(heading);
+                    }
+                    if(i==1){
+                        const heroTxt = document.createElement('p');
+                        heroTxt.classList.add('heroTxt');
+                        heroTxt.textContent = li.textContent;
+                        updates.appendChild(heroTxt);
+                    }
+                    if(i==2){
+                        const name = document.createElement('p');
+                        name.classList.add('name');
+                        name.textContent = li.textContent;
+                        updates.appendChild(name);
+                    }
+                    if(i==3){
+                        const heading = document.createElement('p');
+                        heading.classList.add('heading');
+                        heading.textContent = li.textContent;
+                        updates.appendChild(heading);
+                    }                    
+                });
+				rightLayout.appendChild(updates);
             }
         });
     });
-   
-	
+    block.innerHTML = '';
+    block.appendChild(leftLayout);
+    block.appendChild(rightLayout);
 }

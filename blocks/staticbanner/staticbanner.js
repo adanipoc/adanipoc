66b68addbcd3c5fb655570bd5dfde6ae59381680
Amit Deshpande <amit.deshpande1@ibm.com>
2025-02-26 16:57:45 +0530

    vedio changes

--- blocks/staticbanner/staticbanner.js
+++ blocks/staticbanner/staticbanner.js
@@ -2,18 +2,8 @@ export default function decorate(block) {
     const banner = document.createElement('div');
     banner.classList.add('banner');
     const video = document.createElement('video');
-    video.classList.add('bannerImage');
-    video.setAttribute('preload', 'auto');
-    video.setAttribute('loop', 'true');
-    video.setAttribute('autoplay', 'true');
-    video.setAttribute('muted','true');
-    const source = document.createElement('source');
-    source.src = block.querySelector('p').textContent;
-    source.setAttribute('type','video/mp4')
-    video.appendChild(source);
-    banner.appendChild(video);
-    block.innerHTML = '';
-    block.appendChild(banner);
-
-    
+    const videoSrc = block.querySelector('p').textContent; // Get the video URL
+    banner.innerHTML = '<video preload="auto" loop="true" autoplay="true" muted><source src="' + videoSrc + '" type="video/mp4"></video>'
+    block.innerHTML = ''; // Clear the content inside the block
+    block.appendChild(banner); // Add the new banner to the block
 }

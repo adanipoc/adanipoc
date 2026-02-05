df47987363a994c0363da0553eec7e095374e8fb
Soura Sankar Mondal <soura.sankar@ibm.com>
2026-02-05 18:26:41 +0530

    changes

--- blocks/icatestblock/icatestblock.js
+++ blocks/icatestblock/icatestblock.js
@@ -1,23 +1,20 @@
 export default function decorate(block) {
-    const items = [];
     const rows = [...block.children];
-    rows.forEach((row) => {
-        const cols = [...row.children];
-        cols.forEach((col) => {
-            const text = col.textContent.trim();
-            if (text.length > 0) {
-                items.push(text);
-            }
-        });
-    });
-    block.innerHTML = '';
     const container = document.createElement('div');
-    container.classList.add('eds-menu');
-    items.forEach((label) => {
-        const item = document.createElement('div');
-        item.classList.add('eds-menu-item');
+    container.classList.add('faq-list');
+    rows.forEach((row, index) => {
+        const cols = [...row.children];
+        const label = cols[0]?.textContent.trim();
+        const link = cols[1]?.querySelector('a')?.href || '#';
+        const item = document.createElement('a');
+        item.classList.add('faq-item');
+        item.href = link;
+        if (index === rows.length - 1) {
+            item.classList.add('see-more');
+        }
         item.textContent = label;
         container.appendChild(item);
     });
+    block.innerHTML = '';
     block.appendChild(container);
 }
